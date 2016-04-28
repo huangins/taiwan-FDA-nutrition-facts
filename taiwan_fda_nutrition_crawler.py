86123ead455ceb77473e50703932eff54bf3ae28
wendellliu <cuk.bas@gmail.com>
2016-04-28 17:41:39 +0800

    save json file

--- taiwan_fda_nutrition_crawler.py
+++ taiwan_fda_nutrition_crawler.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 
 """
@@ -27,10 +28,12 @@ class NutritionCrawler():
         (
         "https://consumer.fda.gov.tw/Food/detail/TFNDD.aspx?f=0&pid=%s"
         )
-        self.pid_range = range(150, 161)
+        self.pid_range = range(0, 3500)
         self.required_nutrition = ['熱量', '水分', '粗蛋白', '粗脂肪'
                                ,'總碳水化合物']
         self.required_entry = '每單位重'
+
+        self.json_file = 'nutrition.json'
 
     def get_one_pid_df(self, pid):
         """
@@ -55,7 +58,7 @@ class NutritionCrawler():
         if table is None:
             raise NoneItemException
 
-        df = pd.read_html(str(table), header=0, index_col=1)[0]
+        df = pd.read_html(str(table), header=0, index_col=1, encoding='utf8')[0]
         df = df.loc[self.required_nutrition].filter(regex=self.required_entry).T.reset_index(drop=True)
         df.loc[:, 'name'] = name
         df.loc[:, 'trivial'] = trivial
@@ -71,11 +74,24 @@ class NutritionCrawler():
 
         df_list = []
         for i in self.pid_range:
+            print(i)
             try:
                 df_list.append(self.get_one_pid_df(str(i)))
+
+                # stop time
+                time.sleep(1)
+
             except NoneItemException:
                 continue
 
         result = pd.concat(df_list).reset_index(drop=True)
 
         return result
+
+
+    def save_to_json_file(self):
+        df = self.get_entrie_df()
+
+        with open(self.json_file, 'w') as f:
+            f.write(df.to_json(force_ascii=False))
+
