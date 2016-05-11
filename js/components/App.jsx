080e9dfb441ea943062ed217ee09457010015c81
wendellliu <cuk.bas@gmail.com>
2016-05-11 17:34:05 +0800

    app ui

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -4,34 +4,46 @@ import CategoryFilter from '../containers/CategoryFilter'
 import VisibleResultList from '../containers/VisibleResultList'
 import PinList from '../containers/PinList'
 import PinDashboardList from '../containers/PinDashboardList'
-import { Row, Col, Tabs, Tab } from 'react-bootstrap'
+import { Row, Col, Tab, Nav, NavItem } from 'react-bootstrap'
 
 const App = () => (
-	<Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
-		<Tab eventKey={1} title="Dashboard">
-			<Row className="tab-page">
-				<Col xs={10} xsOffset={1}>
-					<PinDashboardList />
-				</Col>
-			</Row>
-		</Tab>
-		<Tab eventKey={2} title="Form">
-			<Row className="tab-page">
-				<Col xs={10} xsOffset={1}>
-					<KeywordFilter />
-					<CategoryFilter />
-					<Row>
-						<Col md={6}>
-							<VisibleResultList />
-						</Col>
-						<Col md={6}>
-							<PinList />
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-		</Tab>
-	</Tabs>
+	<Tab.Container defaultActiveKey={2} id="uncontrolled-tab-example">
+	<Row className="app-container">
+		<Col xs={2} className="navbar">
+			<Nav bsStyle="pills" stacked>
+				<NavItem eventKey={1}>
+				Dashboard
+				</NavItem>
+				<NavItem eventKey={2}>
+				Form
+				</NavItem>
+			</Nav>
+		</Col>
+		<Col xs={10} className="tab-page">
+			<Tab.Content animation>
+				<Tab.Pane eventKey={1} title="Dashboard">
+
+							<PinDashboardList />
+
+				</Tab.Pane>
+				<Tab.Pane eventKey={2} title="Form">
+
+							<KeywordFilter />
+							<CategoryFilter />
+							<Row>
+								<Col md={6}>
+									<VisibleResultList />
+								</Col>
+								<Col md={6}>
+									<PinList />
+								</Col>
+							</Row>
+
+				</Tab.Pane>
+			</Tab.Content>
+		</Col>
+	</Row>
+	</Tab.Container>
 )
 
 export default App
