948d068b3482a71f173bb35f54bd93a7ef5cb7e8
Ins Huang <ins.huang@gmail.com>
2016-05-14 12:39:53 +0800

    Add GAE config and handler

---
+++ all_handlers.py
@@ -0,0 +1,26 @@
+import os
+import urllib
+
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
